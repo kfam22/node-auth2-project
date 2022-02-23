491ce18ab83a4d54e1932b661b82fd89ff0c4fa5
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2022-02-22 21:49:53 -0500

    setup

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -44,7 +44,23 @@ function findBy(filter) {
         "role_name": "admin",
       }
     ]
+
+    SELECT 
+    user_id,
+    username,
+    password,
+    role_name
+FROM users
+JOIN roles on
+    users.role_id = roles.role_id
+WHERE users.user_id = 1;
    */
+
+return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'password', 'role_name')
+  .where(filter)
+
 }
 
 function findById(user_id) {
@@ -58,6 +74,11 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+
+    return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'password', 'role_name')
+  .where('users.user_id', user_id).first()
 }
 
 /**
